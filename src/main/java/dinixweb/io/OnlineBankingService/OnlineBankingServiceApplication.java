a10f67ddb61947293a82f1fb46de60214ea146fd
dinixweb <sd4live@gmail.com>
2022-09-25 21:40:36 +0100

    initial commit

---
+++ src/main/java/dinixweb/io/OnlineBankingService/OnlineBankingServiceApplication.java
@@ -0,0 +1,13 @@
+package dinixweb.io.OnlineBankingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dinixweb/io/OnlineBankingService/OnlineBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dinixweb.io.OnlineBankingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
